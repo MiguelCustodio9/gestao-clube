313493847bbc6fe6f7c29abadd65fd754b8ef05b
Miguel Custodio <migacusta9@gmail.com>
2026-06-09 16:37:13 +0100

    Add initial database schema for players, coaches, and season links; define tables and constraints

---
+++ bd.sql
@@ -0,0 +1,44 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
+CREATE TABLE public.jogadoras (
+  id_jogadora integer NOT NULL DEFAULT nextval('jogadoras_id_jogadora_seq'::regclass),
+  nome_desportivo character varying NOT NULL,
+  nome_completo character varying NOT NULL,
+  data_nascimento date NOT NULL,
+  nacionalidade character varying DEFAULT 'Portuguesa'::character varying,
+  posicao USER-DEFINED,
+  pe_preferencial USER-DEFINED,
+  numero_preferido integer,
+  clube_anterior character varying,
+  primeira_epoca character varying,
+  escalao_etario character varying,
+  jogos_clube integer DEFAULT 0,
+  golos_clube integer DEFAULT 0,
+  golos_sofridos integer DEFAULT 0,
+  assistencias_clube integer DEFAULT 0,
+  ativo boolean DEFAULT true,
+  CONSTRAINT jogadoras_pkey PRIMARY KEY (id_jogadora)
+);
+CREATE TABLE public.treinadores (
+  id_treinador integer NOT NULL DEFAULT nextval('treinadores_id_treinador_seq'::regclass),
+  nome_desportivo character varying NOT NULL,
+  nome_completo character varying NOT NULL,
+  data_nascimento date NOT NULL,
+  nacionalidade character varying DEFAULT 'Portuguesa'::character varying,
+  clube_anterior character varying,
+  primeira_epoca character varying,
+  ativo boolean DEFAULT true,
+  CONSTRAINT treinadores_pkey PRIMARY KEY (id_treinador)
+);
+CREATE TABLE public.vinculo_epocas (
+  id_vinculo integer NOT NULL DEFAULT nextval('vinculo_epocas_id_vinculo_seq'::regclass),
+  id_jogadora integer,
+  id_treinador integer,
+  epoca character varying NOT NULL,
+  escalao_equipa character varying NOT NULL,
+  funcao_treinador USER-DEFINED,
+  CONSTRAINT vinculo_epocas_pkey PRIMARY KEY (id_vinculo),
+  CONSTRAINT vinculo_epocas_id_jogadora_fkey FOREIGN KEY (id_jogadora) REFERENCES public.jogadoras(id_jogadora),
+  CONSTRAINT vinculo_epocas_id_treinador_fkey FOREIGN KEY (id_treinador) REFERENCES public.treinadores(id_treinador)
+);
